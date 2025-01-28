f652065399dab4bc96dfea537308ce8fa2812ffc
Andrey-true <andreymgn184123@gmail.com>
2025-01-29 00:41:18 +0500

    Initial commit

---
+++ 2_2_2-master/src/main/java/web/config/AppConfig.java
@@ -0,0 +1,97 @@
+package web.config;
+
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+
+@Configuration
+@PropertySource("classpath:db.properties")
+@EnableTransactionManagement
+@ComponentScan(value = "web")
+public class AppConfig {
+
+
+    private Environment env;
+
+    @Autowired
+    public AppConfig(Environment env) {
+        this.env = env;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(getDataSource());
+    }
+
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        JpaTransactionManager jpaTransactionManager = new JpaTransactionManager();
+        jpaTransactionManager.setEntityManagerFactory(entityManagerFactory);
+        return jpaTransactionManager;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(getDataSource());
+        em.setPackagesToScan(env.getRequiredProperty("db.entity.package")); /////  ???????????
+        em.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        em.setJpaProperties(getHibernateProperties());
+        return em;
+    }
+
+
+    @Bean
+    public DataSource getDataSource() {
+        BasicDataSource ds = new BasicDataSource();
+        ds.setUrl(env.getRequiredProperty("db.url"));
+        ds.setDriverClassName(env.getRequiredProperty("db.driver"));
+        ds.setUsername(env.getRequiredProperty("db.username"));
+        ds.setPassword(env.getRequiredProperty("db.password"));
+        return ds;
+    }
+
+    public Properties getHibernateProperties() {
+
+
+        try {
+            Properties properties = new Properties();
+            InputStream is = getClass().getClassLoader().getResourceAsStream("hibernate.properties");
+            properties.load(is);
+            return properties;
+        } catch (IOException e) {
+            try {
+                throw new IOException(e);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        }
+
+    }
+
+}
+
+
+
---
+++ 2_2_2-master/src/main/java/web/dao/UserDaoImp.java
@@ -0,0 +1,66 @@
+package web.dao;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import web.model.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Repository
+public class UserDaoImp implements UserDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+
+    @Override
+    public List<User> getListUserFilled() {
+        List<User> users = entityManager
+                .createQuery("SELECT c FROM User c", User.class)
+                .getResultList();
+        return users;
+    }
+
+    @Override
+    public void addUser(User user) {
+        entityManager.persist(user);
+        List<User> users = getListUserFilled();
+    }
+
+    @Override
+    public void updateUser(Long id, User user) {
+        User existingUser = entityManager.find(User.class, id);
+        if (existingUser != null) {
+            existingUser.setName(user.getName());
+            existingUser.setAge(user.getAge());
+            existingUser.setCity(user.getCity());
+            entityManager.merge(existingUser);
+        }
+    }
+
+    @Override
+    public void removeUser(Long id) {
+        User user = entityManager.find(User.class, id);
+        if (user != null) {
+            entityManager.remove(user);
+        }
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        return entityManager.find(User.class, id);
+    }
+
+
+    @Override
+    public List<User> getUsers(int count) {
+        return entityManager.createQuery("SELECT c FROM User c", User.class)
+                .setMaxResults(count)
+                .getResultList();
+    }
+
+}
